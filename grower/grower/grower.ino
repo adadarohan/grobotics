b63d1f06448575bdca92c01e9ac4e1d9194eee4a
Rohan Kapur <adada.rohan@gmail.com>
2021-05-19 13:03:34 +0530

    added thinkspeak integration

--- grower/grower/grower.ino
+++ grower/grower/grower.ino
@@ -1,25 +1,34 @@
-//Grobotics Arduino code
-//#include <SoftwareSerial.h>
+//Grobotics NodeMCU code
 
-#include "DHT.h"        
+#include <ESP8266WiFi.h>;
+#include <ThingSpeak.h>;
+#include "DHT.h"  
+
+//DHT2 is for the lower chamber
+      
 #define DHTTYPE DHT11   
-
 #define dht_dpin 2
 #define dht_dpin2 13
 
 DHT dht(dht_dpin, DHTTYPE); 
 DHT dht2(dht_dpin2, DHTTYPE); 
 
-//DHT2 is for the lower chamber
+// WiFi Setup
+
+const char* ssid = "y2how2"; //Your Network SSID
+const char* pass = "nonadubak"; //Your Network Password
+
+WiFiClient client;
+unsigned long myChannelNumber = 1148581; //Your Channel Number (Without Brackets)
+const char * myWriteAPIKey = "XJSKW6QO5AR3KCLX"; //Your Write API Key
 
 //Variabes
-int plantdata[] = {50, 29.5 , 255 , 0 , 255, 90};  // Top Humidity, Temperature, R, G, B, Bottom Humidity
+int plantdata[] = {50, 29.5 , 255 , 0 , 255, 90};  //  Temperature,Top Humidity, R, G, B, Bottom Humidity
+int state[] = {0,0,0,0,0,0} ; //Temperature, Top Humidity, R, G, B, Bottom State
+
 //Pins
 
-/*
-int spray = 8;
-*/
-
+//int spray = 8;
 int water = D6;
 int fan = D0;
 int red = D1;
@@ -31,33 +40,61 @@ void setup() {
   Serial.begin(115200);
   dht.begin();
   dht2.begin();
-
+  
   pinMode(red, OUTPUT);
   pinMode(green, OUTPUT);
   pinMode(blue, OUTPUT);
   pinMode(fan, OUTPUT);
   pinMode(heat, OUTPUT);
   pinMode(water, OUTPUT);
+  //pinMode(spray, OUTPUT);
 
-  /*
-  pinMode(spray, OUTPUT);
-
-  */
+  // Connect to WiFi network
+  
+  Serial.println("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, pass);
+  
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+      delay(500);
+      Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  ThingSpeak.begin(client);
+  
   //Safety delay
   delay(50);
 }
  
 void loop() {
   setLight(plantdata[2],plantdata[3],plantdata[4]);
-  setTemp(plantdata[1]);
+  setTemp(plantdata[0]);
   cycle(plantdata[5]);
-  delay(2000);
+  
+  //Upload to thingspeak
+  for(int y = 1; y < 7; y++){
+      ThingSpeak.setField(y, state[y-1]);
+  }
+
+  int r = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+  if(r == 200){
+    Serial.println("Channel update successful.");
+  }
+  else{
+    Serial.println("Problem updating channel. HTTP error code " + String(r));
+  }
+  delay(30000);
 }
 
 void setLight( float r, float g , float b){
   analogWrite(red, (r/255) * 1023 );
   analogWrite(green, (g/255) * 1023);
   analogWrite(blue, (b/255) * 1023);
+  state[2] = r ;
+  state[3] = g ;
+  state[4] = b ;
 }
 
 /*
@@ -77,19 +114,20 @@ void setHum ( int rh) {
 void setTemp (float rt){
   // Get current temperature (ct)
   float ct = dht.readTemperature();
-  //Serial.println(ct);
+  float ch = dht.readHumidity();
 
+  state[0] = ct;
+  state[1] = ch;
+  
   //Heat and fan are triggered by low
   
   if ( rt < ct ) {
       digitalWrite(fan, LOW); //Fan on
       digitalWrite(heat, HIGH); //Heat off
-      Serial.println("Fan on, Heat off");
   }
   else {
       digitalWrite(fan, HIGH); //Fan off
       digitalWrite(heat, LOW); //Heat on
-      Serial.println("Fan off, Heat on");
    }
   
 }
@@ -98,12 +136,10 @@ void setTemp (float rt){
 void cycle (float rh) {
     float ch = dht2.readHumidity();
     if ((int)ch < (int)rh){
-      Serial.println("Water on");
       digitalWrite(water, LOW);
-      delay(10000);
-      digitalWrite(water, HIGH);
+      state[5] = 1;
     } else {
-      Serial.println("Water off");
       digitalWrite(water, HIGH);
+      state[5] = 0;
     }
 }
